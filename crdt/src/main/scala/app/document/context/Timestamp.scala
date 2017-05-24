23d5f8a8f3644cb8484df9e96022004a51b9a1d0
Victor Axelsson <victor_axelsson@hotmail.com>
2017-05-24 15:20:40 +0200

    Fixed some bug with timestamp overriding hashCode and equals

--- crdt/src/main/scala/app/document/context/Timestamp.scala
+++ crdt/src/main/scala/app/document/context/Timestamp.scala
@@ -3,10 +3,13 @@ package app.document.context
 /**
   * Created by victoraxelsson on 2017-05-24.
   */
-class Timestamp(c:Int, p:Int) {
+class Timestamp(c:Int, p:Int){
 
   def getC() = c
   def getP() = p
+
+
+
    def isGreaterThan(ts:Timestamp): Boolean= {
     if(p == ts.getP()){
       return c > ts.getC()
@@ -23,7 +26,16 @@ class Timestamp(c:Int, p:Int) {
     return c < ts.getC()
   }
 
-   def equals(obj: Timestamp): Boolean = {
-    return (obj.getP() == p) && (obj.getC() == c)
+
+  override def equals(obj: scala.Any): Boolean = {
+    if(!obj.isInstanceOf[Timestamp]){
+      return false
+    }
+
+    return (obj.asInstanceOf[Timestamp].getP() == p) && (obj.asInstanceOf[Timestamp].getC() == c)
+  }
+
+  override def hashCode(): Int = {
+    (c + ":" + p).hashCode
   }
 }
